ee7a9a5d8ba93e01276466aeea6f884501b77663
JesseWK100 <j.walusansa@alustudent.com>
2025-05-19 12:27:49 +0200

    save

--- models/user.py
+++ models/user.py
@@ -1,14 +1,18 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+"""Defines the User class."""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """Class representing a user."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize a new User."""
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get("email", "")
-        self.password = kwargs.get("password", "")
-        self.first_name = kwargs.get("first_name", "")
-        self.last_name = kwargs.get("last_name", "")
+    """Represent a User.
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
